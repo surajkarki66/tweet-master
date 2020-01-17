ff46d1af2569af71984a763c8955fecac212b5cd
surajkarki66 <suraj.karki500@gmail.com>
2020-01-17 16:03:40 +0545

    Finished like tweet

--- tweet.py
+++ tweet.py
@@ -11,21 +11,45 @@ class Tweet:
 
     def user_tweets(self):
         username = 'Surazz karkey'
-        self.user_stat = self.api.user_timeline(username)
-        
+        user_stat = self.api.user_timeline(username)
+        for i in user_stat:
+            status_id = i.id
+            self.api.create_favorite(status_id)
+            print("Liked")
 
         user_tweet = [{'text': i.text, 'likes': i.favorite_count, 'retweet': i.retweet_count,
-                        'isRetweeted': i.retweeted, 'reply_to': i.in_reply_to_screen_name,
-                        'created_at': i.created_at} for i in self.user_stat]
+                       'isRetweeted': i.retweeted, 'reply_to': i.in_reply_to_screen_name,
+                       'created_at': i.created_at} for i in user_stat]
 
         return user_tweet
 
-
     def followers_tweet(self):
-        self.tweets = self.api.home_timeline()
-
-        tweets = [{'text':i.text, 'author':i.user.name,
-                    'likes':i.favorite_count, 'created_at': i.created_at} for i in self.tweets]
+        tweets = self.api.home_timeline()
+        tweets = [{'text': i.text, 'author': i.user.name,
+                   'likes': i.favorite_count, 'created_at': i.created_at} for i in tweets]
         return tweets
-      
 
+    def like_tweet(self):
+        tweets = self.api.home_timeline()
+        try:
+            for i in tweets:
+                status_id = i.id
+                self.api.create_favorite(status_id)
+                print("Liked->", i.text, "By ", i.user.name)
+
+        except t.TweepError as e:
+            print(e.reason)
+
+        finally:
+            for i in tweets:
+                print("Text", i.text, "BY->", i.user.name)
+                cmd = input("Press d to dislike individual tweets\nPress s to dislike all tweets")
+                if cmd == "d":
+                    status_id = i.id
+                    self.api.destroy_favorite(status_id)
+
+                elif cmd == "s":
+                    self.api.destroy_favorite(status_id)
+
+                else:
+                    return
