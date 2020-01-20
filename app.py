6d82dd52b0faab056ddb7846cdad9d736c362543
surajkarki66 <suraj.karki500@gmail.com>
2020-01-20 16:18:13 +0545

    Finished tweet master v1

--- app.py
+++ app.py
@@ -13,7 +13,19 @@ class Menu:
             "2": self.show_followers,
             "3": self.show_following,
             "4": self.show_messages,
-            "6": self.quit
+            "5": self.send_message_all,
+            "6": self.send_individual,
+            "7": self.user_tweets,
+            "8": self.followers_tweets,
+            "9": self.user_retweets,
+            "10": self.specified_user_tweets,
+            "11": self.like_all,
+            "12": self.like_specified,
+            "13": self.retweet_all,
+            "14": self.retweet_specified,
+            "15": self.reply_all,
+            "16": self.reply_specific,
+            "17": self.quit
 
         }
 
@@ -25,7 +37,19 @@ class Menu:
         2. To see Your Follower
         3. To see Your Following
         4. To see Your Messages
-        5. To exit
+        5. To send message to all
+        6. To send message individually
+        7. To see user tweets
+        8. To see friends tweets
+        9. To see retweets of me
+        10. To see specified user tweets
+        11. To like all tweets
+        12. To like specified user status
+        13. To retweet all home status
+        14. To retweet specified user
+        15. To reply all post
+        16. To reply specific post
+        17. To quit
         """)
 
     def run(self):
@@ -74,6 +98,91 @@ class Menu:
             print('Receiver:', m['receivers'])
             print('Messages:', m['messages'])
             print('')
+
+    def send_message_all(self):
+        text = str(input("Enter the message:"))
+        self.profile.post_messages(text)
+        print('Message successfully sent.')
+
+    def send_individual(self):
+        text = str(input("Enter the message:"))
+        r_id = str(input("Enter the recipient id:"))
+        self.profile.send_individual(text, r_id)
+        print('Message successfully sent.')
+
+    def user_tweets(self):
+        print("Your tweets are:")
+        tweet = self.tweet.user_tweets()
+        for t in tweet:
+            print('Text:', t['text'])
+            print('Likes:', t['likes'])
+            print('Retweet_Count:', t['retweet'])
+            print('Is Retweeted:', t['isRetweeted'])
+            print('Reply To:', t['reply_to'])
+            print('Created At:', t['created_at'])
+            print('')
+
+    def followers_tweets(self):
+        print("Your friends tweets are")
+        tweet = self.tweet.followers_tweet()
+        for t in tweet:
+            print('Text:', t['text'])
+            print('Likes:', t['likes'])
+            print('Author:', t['author'])
+            print('Created At:', t['created_at'])
+            print('')
+
+    def user_retweets(self):
+        print("Your Retweets are:")
+        retweets = self.tweet.retweets_of_me()
+        for t in retweets:
+            print('Text:', t['text'])
+            print('Likes:', t['likes'])
+            print('Author:', t['author'])
+            print('Created At:', t['created_at'])
+            print('')
+
+    def specified_user_tweets(self):
+        name = str(input("Enter the username:"))
+        print(f'Tweets of {name} are:')
+        tweets = self.tweet.get_specified_user_status(name)
+        for t in tweets:
+            print('Text:', t['text'])
+            print('Likes:', t['likes'])
+            print('Author:', t['author'])
+            print('Created At:', t['created_at'])
+            print('')
+
+    def like_all(self):
+        like = self.tweet.like_tweet()
+        print("Liked")
+
+    def like_specified(self):
+        name = str(input("Enter the name:"))
+        self.tweet.like_specified_user_status(name)
+        print(f'Liked {name} status ')
+
+    def retweet_all(self):
+        self.tweet.retweet()
+
+    def retweet_specified(self):
+        name = str(input("Enter the name:"))
+        self.tweet.retweet_specified_user_status(name)
+        print(f'Retweeted to {name} post')
+
+    def reply_all(self):
+        message = input("Enter the message:")
+        self.tweet.reply_to_tweet(message)
+        print('Replied all')
+
+    def reply_specific(self):
+        name = input("Enter the name:")
+        message = input("Enter the message:")
+        self.tweet.reply_to_specified_user_tweet(message, name)
+
+
+
+
 
     def quit(self):
         sys.exit(1)
--- main/tweet.py
+++ main/tweet.py
@@ -11,70 +11,70 @@ class Tweet:
 
     def user_tweets(self):
         username = 'Surazz karkey'
-        user_stat = self.api.user_timeline(username, count=10)
-        user_tweet = [{'text': i.text, 'likes': i.favorite_count, 'retweet': i.retweet_count,
+        user_stat = self.api.user_timeline(username, count=20, tweet_mode="extended")
+        user_tweet = [{'text': i.full_text, 'likes': i.favorite_count, 'retweet': i.retweet_count,
                        'isRetweeted': i.retweeted, 'reply_to': i.in_reply_to_screen_name,
                        'created_at': i.created_at} for i in user_stat]
 
         return user_tweet
 
     def followers_tweet(self):
-        tweets = self.api.home_timeline()
-        tweets = [{'text': i.text, 'author': i.user.name,
+        tweets = self.api.home_timeline(count=20, tweet_mode="extended")
+        tweets = [{'text': i.full_text, 'author': i.user.name,
                    'likes': i.favorite_count, 'created_at': i.created_at} for i in tweets]
         return tweets
 
     def retweets_of_me(self):
         retweets = self.api.retweets_of_me()
-        retweet = [{'text': i.text, 'auther': i.user.name,
+        retweet = [{'text': i.text, 'author': i.user.name,
                     'likes': i.favorite_count, 'created_at': i.created_at} for i in retweets]
 
         return retweet
 
     def get_specified_user_status(self, screenname):
-        tweets = self.api.user_timeline(screenname)
-        tweets = [{'status_id': i.id, 'text': i.text, 'author': i.user.name,
+        tweets = self.api.user_timeline(screenname, tweet_mode="extended")
+        tweets = [{'status_id': i.id, 'text': i.full_text, 'author': i.user.name,
                    'likes': i.favorite_count, 'created_at': i.created_at} for i in tweets]
         return tweets
 
     # POST
     def like_tweet(self):
-        tweets = self.api.home_timeline()
+        tweets = self.api.home_timeline(tweet_mode="extended")
         for i in tweets:
             try:
                 status_id = i.id
                 self.api.create_favorite(status_id)
-                print("Liked->", i.text, "By ", i.user.name)
+                print("Liked->", i.full_text, "By ", i.user.name)
 
             except t.TweepError as e:
                 pass
 
     def retweet(self):
-        tweets = self.api.home_timeline()
+        tweets = self.api.home_timeline(tweet_mode="extended")
         for i in tweets:
             try:
                 status_id = i.id
                 self.api.retweet(status_id)
-                print("retweeted on", i.text)
+                print("retweeted on", i.full_text)
 
             except t.TweepError as e:
                 print(e.reason)
                 pass
 
     def unretweeted(self):
-        tweets = self.api.home_timeline()
+        tweets = self.api.home_timeline(tweet_mode="extended")
         for i in tweets:
             try:
                 status_id = i.id
                 self.api.unretweet(status_id)
-                print('Unretweeted on', i.text)
+                print('Unretweeted on', i.full_text)
 
             except t.TweepError as e:
                 print(e.reason)
                 pass
 
     def reply_to_tweet(self, reply_message):
-        tweets = self.api.home_timeline()
+        tweets = self.api.home_timeline(tweet_mode="extended")
         for i in tweets:
             try:
                 status_id = i.id
@@ -85,18 +85,18 @@ class Tweet:
                 pass
 
     def like_specified_user_status(self, screenname):
-        tweets = self.api.user_timeline(screenname)
+        tweets = self.api.user_timeline(screenname, tweet_mode="extended")
         for i in tweets:
             try:
                 status_id = i.id
                 self.api.create_favorite(status_id)
-                print("Liked->", i.text, "By ", i.user.name)
+                print("Liked->", i.full_text, "By ", i.user.name)
 
             except t.TweepError as e:
                 pass
 
     def retweet_specified_user_status(self, screenname):
-        tweets = self.api.user_timeline(screenname)
+        tweets = self.api.user_timeline(screenname, tweet_mode="extended")
         for i in tweets:
             try:
                 status_id = i.id
@@ -107,11 +107,12 @@ class Tweet:
                 pass
 
     def reply_to_specified_user_tweet(self, reply_message, screenname):
-        tweets = self.api.user_timeline(screenname)
+        tweets = self.api.user_timeline(screenname, tweet_mode="extended")
         for i in tweets:
             try:
                 status_id = i.id
                 self.api.update_status(reply_message, status_id, auto_populate_reply_metadata=True)
+                print('Replied to ', i.full_text)
 
             except t.TweepError as e:
                 print(e.reason)
